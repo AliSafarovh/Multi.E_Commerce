70857eee15a4a0aeeb6271bcc645e515646e109f
Ali Safaroff <ali@example.com>
2025-01-17 16:25:21 +0400

    addd

--- Services/Catalog/MultiShop.Catalog/Dtos/CategoryDtos/CreateCategoryDto.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace MultiShop.Catalog.Dtos.CategoryDtos;
-
-public class CreateCategoryDto
-{
-    public string? CategoryName { get; set; }
-}
--- Services/Catalog/MultiShop.Catalog/Dtos/CategoryDtos/ResultCategoryDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MultiShop.Catalog.Dtos.CategoryDtos;
-
-public class ResultCategoryDto
-{
-    public string? CategoryId { get; set; }
-    public string? CategoryName { get; set; }
-}
